8b63fb18c58ac388a51b3727ef0f2a5d442a3799
Dimitrius <shimansky.dmitry@gmail.com>
2025-06-07 14:17:10 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import {Provider} from 'react-redux'
+import {AppHttpRequests} from './app/AppHttpRequests.tsx'
+import {store} from './app/store.ts'
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+      <AppHttpRequests/>
+    </Provider>
+)
