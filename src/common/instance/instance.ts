2f74b155828608ffc67f094d890836a08cf23a55
Dimitrius <shimansky.dmitry@gmail.com>
2025-08-03 15:45:13 +0300

    Sprint 03 Week 04

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,9 @@
-import axios from "axios";
-
-const token = '66ad4bee-3015-45dc-92d1-d5f5c14fc919'
-const apiKey = 'e2dbdc3e-bea2-44f2-a873-dd87cb2109cc'
+import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1',
-    headers: {
-        Authorization: `Bearer ${token}`,
-        'API-KEY': apiKey,
-    }
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    "API-KEY": import.meta.env.VITE_API_KEY,
+  },
 })
